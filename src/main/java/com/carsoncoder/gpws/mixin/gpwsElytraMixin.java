c736027376522a903a5c6135045de07f414e9b1b
carson-coder <carsondpool@gmail.com>
2023-08-14 09:19:19 -0400

    FIX

--- src/main/java/com/carsoncoder/gpws/mixin/gpwsElytraMixin.java
+++ src/main/java/com/carsoncoder/gpws/mixin/gpwsElytraMixin.java
@@ -1,5 +1,8 @@
 package com.carsoncoder.gpws.mixin;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
