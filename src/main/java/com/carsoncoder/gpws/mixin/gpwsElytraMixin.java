fc66e60456f2019a6645253aa888ed1022098864
carson-coder <carsondpool@gmail.com>
2023-08-14 09:18:54 -0400

    FIX

--- src/main/java/com/carsoncoder/gpws/mixin/gpwsElytraMixin.java
+++ src/main/java/com/carsoncoder/gpws/mixin/gpwsElytraMixin.java
@@ -8,6 +8,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
 public class gpwsElytraMixin {
+    public static final Logger LOGGER = LoggerFactory.getLogger("gpws-elytra");
+
 	@Inject(at = @At("HEAD"), method = "loadWorld")
 	private void init(CallbackInfo info) {
 		// This code is injected into the start of MinecraftServer.loadWorld()V
